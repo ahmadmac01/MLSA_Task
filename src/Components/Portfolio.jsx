c5bd132944e5e4167a5e5fde6f4c9467dbb8ff9b
Muhammad Ahmad <ahmadmac510@gmail.com>
2023-09-22 20:44:16 +0500

    updated

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/background1.jpeg";
+import image from "../images/background1.jpg";
 
-const imageAltText = " Background photo on desktop with a cup of coffee and laptop";
+const imageAltText = " Background photo on desktop with a cup of coffee and laptop.";
 
 /**
  * Project list
